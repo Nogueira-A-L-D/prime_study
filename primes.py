06288b6149065033d4f04bda5517a23382a7442e
Nogueira-A-L-D <105943852+Nogueira-A-L-D@users.noreply.github.com>
2025-05-24 22:24:48 -0300

    Added the check for x position
    Created the inverse function using Newton-Raphson algorithm and applied it to check the x interval in the function.

--- primes.py
+++ primes.py
@@ -8,12 +8,27 @@ from sympy.ntheory.generate import primerange, nextprime
 
 # Mathematical function to be used to create a margin area that always have primes
 class LogFunction:
-    
+
+    e=2.718281828459045235360 # Euler's number
+    max_iteration=10 # Limit of iterations in Newton-Raphson algorithm
+
     def __init__(self, base):
         self.base = base
 
+    def ln(self,x):
+        return math.log(x,self.e)
+
     def on(self, x):
-        return math.log(x, self.base) * x + (1/x) + (1)
+        return math.log(x, math.sqrt(self.base)) * x + (1/x) + (1)
+    
+    def derivate(self, x):
+        return (2*self.ln(x)+2)/self.ln(self.base) - (1/x**2)
+    
+    def inverse(self, count, prime):
+        x = count
+        for i in range(self.max_iteration):
+            x = x-(self.on(x) - prime)/self.derivate(x)
+        return x  
 
 # Enable/disable autosave
 autosave = False
@@ -29,15 +44,21 @@ primes_quantity = int(input("How much prime numbers? :> "))
 # Inicializing functions and the prime list
 initial_primes_time = time.time()
 print("Getting Primes")
-f_superior = LogFunction(math.sqrt(5))
+
+f_superior = LogFunction(5)
 primes = list(primerange(1, nextprime(1,primes_quantity)+1))
-f_inferior = LogFunction(math.sqrt(7))
+f_inferior = LogFunction(7)
+
 print("Primes Obtained\n")
 final_primes_time = time.time()
 
 # Creating lists to save data and find failures
-sup_list = []
-inf_list = []
+sup_list_y = []
+inf_list_y = []
+
+sup_list_x = []
+inf_list_x = []
+
 margin_failure = "No"
 count = 1
 
@@ -46,13 +67,22 @@ initial_test_time = time.time()
 print("Beggining test...")
 for i in primes:
     
+    # Finding the nearest value for prime in each function
     sup_y = f_superior.on(count)
     inf_y = f_inferior.on(count)
 
-    sup_list.append(sup_y)
-    inf_list.append(inf_y)
+    sup_list_y.append(sup_y)
+    inf_list_y.append(inf_y)
 
-    if (sup_y<i or i<inf_y): # If the margin fails, mark on log or shell
+    # Finding the position in which the prime number is, using
+    # the inverse of the function by Newton-Raphson algorithm
+    sup_x = f_superior.inverse(count,i)
+    inf_x = f_inferior.inverse(count,i)
+
+    sup_list_x.append(sup_x)
+    inf_list_x.append(inf_x)
+
+    if (sup_y<i or i<inf_y or sup_x>count or inf_x<count): # If the margin fails, mark on log or shell
         print("Fail: margin doesn't contain the value.")
         margin_failure = "Yes"
         break
@@ -60,6 +90,7 @@ for i in primes:
 
     percentage = (count/primes_quantity)*100
     old_percentage = ((count-1)/primes_quantity)*100
+
     if int(percentage) > int(old_percentage):
         print(str(int(percentage)) + "%")           # Users can see the progress with this percentage
 final_test_time = time.time()
@@ -73,13 +104,22 @@ if not autosave:
 # Saving if enabled
 initial_save_time = time.time()
 if save=='y' or autosave:
+
     # Save informations in a .txt archive separated by commas
     with open("results/primes.txt", "w") as f:
         f.write(','.join(map(str, primes)))
-    with open("results/superior.txt", "w") as f:
-        f.write(','.join(map(str,sup_list)))
-    with open("results/inferior.txt", "w") as f:
-        f.write(','.join(map(str,inf_list)))
+    
+    with open("results/superior_primes.txt", "w") as f:
+        f.write(','.join(map(str,sup_list_y)))
+    
+    with open("results/superior_count.txt", "w") as f:
+        f.write(','.join(map(str,sup_list_x)))
+    
+    with open("results/inferior_primes.txt", "w") as f:
+        f.write(','.join(map(str,inf_list_y)))
+    
+    with open("results/inferior_count.txt", "w") as f:
+        f.write(','.join(map(str,inf_list_x)))
 final_save_time = time.time()
 
 
@@ -93,6 +133,6 @@ with open("results/log.txt", "a+") as f:
 # To close the virtual enviroment, just use "deactivate"
 
 # Conclusion:
-# Until now, all prime values are inside the margin, being necessary to create the inverse of this function to check if all x positions
-# are inside the limits of these functions, confirming that we can find a function that should approximate the prime distribution and,
-# furthemore, could be used to find the function that describe primes, being usefull to improve the decodification of encrypted data.
+# Until now, all prime values are inside the margin x and y of the functions, showing that these two functions can create a margin where
+# all primes are inside, confirming we can find a function that should approximate a prime distribution and could be used to find a
+# function that describe primes, being usefull to improve the decodification of encrypted data.
